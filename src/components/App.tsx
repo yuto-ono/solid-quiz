83015eb9a69dff3318d800c88d20f4d63d5302bb
Yuto Ono <stellarwindmaster@gmail.com>
2022-10-13 11:44:07 +0900

    markup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,50 @@
 import { css } from "@emotion/css"
+import "../global.css"
 
 const App = () => {
   return (
-    <div
+    <section
       class={css`
-        font-size: 40px;
-        color: red;
+        width: 400px;
+        margin: 16px auto;
+        background-color: #fff;
+        border-radius: 4px;
+        padding: 16px;
+        position: relative;
       `}
     >
-      Hello Solid
-    </div>
+      <p
+        class={css`
+          margin-bottom: 16px;
+          font-weight: bold;
+        `}
+      >
+        問題
+      </p>
+      <ul
+        class={css`
+          list-style: none;
+          padding: 0;
+          margin-bottom: 16px;
+        `}
+      >
+        <li class={optionStyle}>選択肢 1</li>
+        <li class={optionStyle}>選択肢 2</li>
+        <li class={optionStyle}>選択肢 3</li>
+      </ul>
+    </section>
   )
 }
 
 export default App
+
+const optionStyle = css`
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+  &:hover {
+    background: #f8f8f8;
+  }
+`
